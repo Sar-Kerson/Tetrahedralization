95c5ab4aba45828c5c35f67a9b2427f347eeeec4
Sar-Kerson <512529447@qq.com>
2018-04-04 04:44:46 +0800

    second commit

---
+++ build/core/demo/CMakeFiles/by_loading.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/by_loading.dir/by_loading/by_loading.cpp.o"
+  "../../../bin/by_loading.pdb"
+  "../../../bin/by_loading"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/by_loading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
